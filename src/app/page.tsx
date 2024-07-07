28ed63d29de054c5478c7ec54bf64e420ad3c5ef
VivekEngima <vivek.vyas.developer@gmail.com>
2024-07-08 03:54:31 +0530

    Day 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (<>
-    <h1>Week 1 Day 1  </h1>
-    <p>I think i have made kind of great and detailed plan to make $10,000 per month in 3 month time</p>
-    <hr />
-  </>
+  return (
+    <>
+      <h1>Non-Negotiable things to do:</h1>
+      <ul>
+        <li>Surya Kriya (min. 3 cycles) + Yogasanas (30 sec)</li>
+        <li>Journaling</li>
+      </ul>
+      <hr />
+      <h1>Day 1 Sunday</h1>
+      <p>
+        I think i have made kind of great and detailed plan to start make
+        $10,000 per month in 3 month time
+      </p>
+
+      <hr />
+      <h1>Day 2 Monday</h1>
+      <hr />
+    </>
   );
 }
